cee07efd00fefa8b5331b3751b7f894cd7ddaffa
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-04-09 06:36:04 +0530

    corrected margins and navbar, added links and increased responsiveness

--- maintainer.js
+++ maintainer.js
@@ -1,15 +1,18 @@
 var myNav = document.getElementsByClassName('header');
 var logo =document.getElementsByClassName('img-logo');
+var svg =document.getElementsByClassName('img-svg');
 window.onscroll = function () { 
     "use strict";
     if (document.body.scrollTop >= 2 || document.documentElement.scrollTop >= 2 ) {
         myNav[0].classList.add("scrolled");
         myNav[0].classList.remove("unscrolled");
         logo[0].classList.add("invisible");
+        svg[0].classList.add("visible");
     } 
     else {
         myNav[0].classList.add("unscrolled");
         myNav[0].classList.remove("scrolled");
         logo[0].classList.remove("invisible");
+        svg[0].classList.remove("visible");
     }
 };
